2952755b1d54db3c7564bf4777f395a7cde12924
emustee15@jcu.edu <emustee15@jcu.edu@c3e5abcf-93a6-cf5b-de93-216d6c25b747>
2014-03-26 23:27:51 +0000

    If you have the axe of the wind you can now make normal trees go faster by spamming the axe of the wind on a tree.

--- TextMyTopping/src/ideasPackage/StaticObject.java
+++ TextMyTopping/src/ideasPackage/StaticObject.java
@@ -13,17 +13,24 @@ public class StaticObject implements Collidable
 	private int animationDuration;
 	private int currentAnimationFrame;
 	private Dialog dialog;
+	private String imageName;
+	
+	private int interactionDurationTimer = 0;
+	private int previousAnimationDuration = 0;
+	private final static int SPECIAL_ANIMATION_DURATION = 120;
 
 	public StaticObject(GridCoordinate coordinates, String imageName, CollisionGrid c, int animationFrames,
-			int anationDuration, boolean addToGrid)
+			int anamationDuration, boolean addToGrid)
 	{
 		this(coordinates, c, addToGrid);
-		this.animationDuration = anationDuration;
+		this.animationDuration = anamationDuration;
 		images = new PImage[animationFrames];
+		this.imageName = imageName;
 		for (int index = 0; index < animationFrames; index++)
 		{
 			images[index] = parent.loadImage("\\data\\sprites\\scenery\\" + imageName + "\\" + index + ".png");
 		}
+		previousAnimationDuration = anamationDuration;
 
 	}
 
@@ -58,11 +65,20 @@ public class StaticObject implements Collidable
 	private void updateAnimation()
 	{
 		currentAnimationFrame++;
-		if (currentAnimationFrame == animationDuration)
+		if (currentAnimationFrame >= animationDuration)
 		{
 			animationIndex++;
 			animationIndex %= images.length;
 			currentAnimationFrame = 0;
+		}
+		
+		if (interactionDurationTimer > 0)
+		{
+			interactionDurationTimer--;
+			if (interactionDurationTimer == 0)
+			{
+				this.animationDuration = previousAnimationDuration;
+			}
 		}
 	}
 
@@ -87,6 +103,15 @@ public class StaticObject implements Collidable
 		if (dialog!=null && interactionId == PlayerCharacter.MAIN_INTERACTION)
 		{
 			dialog.showDialog();
+		}
+		
+		if (imageName.contains("tree") || imageName.contains("cactus"))
+		{
+			if (GlobalBooleanManager.getValue("hasWindAxe"))
+			{
+				this.animationDuration--;
+				interactionDurationTimer = SPECIAL_ANIMATION_DURATION;
+			}
 		}
 	}
 
