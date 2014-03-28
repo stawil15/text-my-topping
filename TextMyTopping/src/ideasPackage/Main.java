73145f23e517118ea5c8ce8078761fcac0ce056f
emustee15@jcu.edu <emustee15@jcu.edu@c3e5abcf-93a6-cf5b-de93-216d6c25b747>
2014-03-28 04:29:42 +0000

    Minor fix in dialog

--- TextMyTopping/src/ideasPackage/Main.java
+++ TextMyTopping/src/ideasPackage/Main.java
@@ -74,7 +74,7 @@ public class Main extends PApplet
 		choices.add("Exit");
 		MainMenu menu = new MainMenu(loadImage("/data/sprites/MainMenu.png"),choices);
 		
-		enterName = new Dialog(new String[] {"Hello good sir or madam! What is your name?"});
+		enterName = new Dialog(new String[] {"Hello good sir or madam! What is your name?\n(Press spacebar to continue)"});
 		
 		ValueSetStringDialog namePrompt = new ValueSetStringDialog("yourName", "What is your name?", "Enter a name.");
 		enterName.setNextDialog(namePrompt);
